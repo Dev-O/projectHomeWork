90de2ebfc30d8000fc47ea41e596d9b4e9756d78
OLAMIDE <OLAMIDE@192.168.2.10>
2016-08-16 05:59:55 -0400

    Initial commit

---
+++ developerHomework/src/main/java/com/ola/DeveloperHomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.ola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperHomeworkApplication.class, args);
+	}
+}
